9ce774f528c34c868711d4e287ca389865214b80
Juan Basgall <juanangelbasgall@hotmail.com>
2023-04-26 12:54:37 -0300

    02 - Limpieza del proyecto y nuestro primer COMPONENTE

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div>
+
+      <h1>Primer componente en React</h1>
+      <Hello></Hello>
+      <Hello></Hello>
+      <Hello></Hello>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+        <p>Este es nuestro primer componente</p>
+        <ul>
+            <li>List item</li>
+            <li>List item</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Hello
