9071f9a27171351be95819fd4284868b87681cfc
Juan <juanangelbasgall@hotmail.com>
2023-05-18 12:57:30 -0300

    Avanzando al ejercicio 21 y arregland la redireccion del host con el archivo _redirect

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,15 +4,15 @@ const Inicio = () => {
   return (
     <div>
         <h1>Estás en el componente Inicio</h1>
-        <h1>Bienvenid@ a mi página d epruebas de React.js</h1>
+        <h1>Bienvenid@ a mi página de pruebas de React.js</h1>
         <hr />
         <br />
-        <p>Esta es un página en la que estoy practicando y aprendiendo al tecnología de react.js</p>
+        <p>Esta es una página en la que estoy practicando y aprendiendo al tecnología de react.js</p>
         <br />
         <p>En este proyecto no hay diseño visual, solo lo mínimo para que la aplicación sea entendible.
           La importancia de la misma es solo es estudio funcional de cada uno de los ejercicios y funcionalidades de React.
         </p>
-        <ul>
+        <ul className='listaInicio'>
           <label htmlFor="">En la misma vemos tecnologias como:</label>
           <li>yarn</li>
           <li>Bootstrap</li>
