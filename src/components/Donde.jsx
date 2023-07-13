6f28cb978393c31e7467fa3a3d9f1f445b290a17
Juan <juanangelbasgall@hotmail.com>
2023-07-12 22:17:26 -0300

    Cambios de texto en donde

--- src/components/Donde.jsx
+++ src/components/Donde.jsx
@@ -4,11 +4,9 @@ const Donde = () => {
   return (
     <div>
         <h1>Estás en el componente Donde</h1>
-        <p>Este es el terrible pedazo de componente donde</p>
-        <p><b>Enrutamiento con parámetros con distintas tecnologías:</b></p>
-        <p>Desde una api con Node = "ruta/:id"</p>
-        <p>Desde React cuando defino las rutas en el componente Route = "path={'/especifico/:id'}"</p>
-        <p>Desde Django en el gestor de urls: "ruta/{'<'}str:nombrebar{'>'}"</p>
+        <h2>Este es el componente donde</h2>
+        <p>Componente de pruebas de enrutamiento.</p>
+        <p>Nada interesante por aquí.</p>
     </div>
   )
 }
